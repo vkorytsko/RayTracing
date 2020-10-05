c8518b56835988759df9d0b83310a62b8cb300c9
Vlad Korytsko <slavkory@gmail.com>
2020-10-05 09:31:23 +0300

    Raycasting
    Simple ray
    Simple camera
    New gradient

--- main.cpp
+++ main.cpp
@@ -8,26 +8,49 @@
 #include "include/stb_image_write.h"
 
 #include "color.h"
+#include "ray.h"
 #include "vec3.h"
+
+
+color ray_color(const ray& r) {
+    vec3 unit_direction = unit_vector(r.direction());
+    auto t = 0.5 * (unit_direction.y() + 1.0);
+    return (1.0 - t) * color(1.0, 1.0, 1.0) + t * color(0.5, 0.7, 1.0);
+}
 
 
 int main() {
 
     // Image
 
-    const int image_width = 256;
-    const int image_height = 256;
-#define CHANNEL_NUM 3
+    const auto aspect_ratio = 16.0 / 9.0;
+    const int image_width = 400;
+    const int image_height = static_cast<int>(image_width / aspect_ratio);
+
+    // Camera
+
+    auto viewport_height = 2.0;
+    auto viewport_width = aspect_ratio * viewport_height;
+    auto focal_length = 1.0;
+
+    auto origin = point3(0, 0, 0);
+    auto horizontal = vec3(viewport_width, 0, 0);
+    auto vertical = vec3(0, viewport_height, 0);
+    auto lower_left_corner = origin - horizontal / 2 - vertical / 2 - vec3(0, 0, focal_length);
 
     // Render
 
+#define CHANNEL_NUM 3
     uint8_t* pixels = new uint8_t[image_width * image_height * CHANNEL_NUM];
 
     int index = 0;
     for (int j = image_height - 1; j >= 0; --j) {
         std::cerr << "\rScanlines remaining: " << j << ' ' << std::flush;
         for (int i = 0; i < image_width; ++i) {
-            color pixel_color(double(i) / (image_width - 1), double(j) / (image_height - 1), 0.25);
+            auto u = double(i) / (image_width - 1);
+            auto v = double(j) / (image_height - 1);
+            ray r(origin, lower_left_corner + u * horizontal + v * vertical - origin);
+            color pixel_color = ray_color(r);
 
             pixels[index++] = to_color(pixel_color.x());
             pixels[index++] = to_color(pixel_color.y());
