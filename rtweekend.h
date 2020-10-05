9bfa8727a5309a99acf0eb9ce96bb5277e564033
Vlad Korytsko <slavkory@gmail.com>
2020-10-05 10:18:11 +0300

    Antialiasing!
    Multisampling;
    Camera class;
    Utility functions.

--- rtweekend.h
+++ rtweekend.h
@@ -4,6 +4,7 @@
 #include <cstdlib>
 #include <limits>
 #include <memory>
+#include <random>
 
 
 // Usings
@@ -21,6 +22,23 @@ const double pi = 3.1415926535897932385;
 
 inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180.0;
+}
+
+inline double random_double() {
+    static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+    static std::mt19937 generator;
+    return distribution(generator);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min,max).
+    return min + (max - min) * random_double();
+}
+
+inline double clamp(double x, double min, double max) {
+    if (x < min) return min;
+    if (x > max) return max;
+    return x;
 }
 
 // Common Headers
