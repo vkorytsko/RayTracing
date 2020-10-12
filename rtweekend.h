2580896a2b186c31ce8ecf0a4f069f1c042006e5
Vlad Korytsko <slavkory@gmail.com>
2020-10-12 09:52:03 +0300

    Diffuse material

--- rtweekend.h
+++ rtweekend.h
@@ -45,3 +45,12 @@ inline double clamp(double x, double min, double max) {
 
 #include "ray.h"
 #include "vec3.h"
+
+
+vec3 random_in_unit_sphere() {
+    while (true) {
+        auto p = vec3::random(-1, 1);
+        if (p.length_squared() >= 1) continue;
+        return p;
+    }
+}
